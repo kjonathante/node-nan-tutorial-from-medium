5c4d26fa9dcf8a22beda6c693ab3ce33a00aad62
Kit Jonathan Te <kjonathante@gmail.com>
2019-05-17 12:36:54 -0700

    Added initial folder structures and files

---
+++ bindings.js
@@ -0,0 +1,9 @@
+let myModule
+
+if (process.env.DEBUG) {
+  myModule= require('./build/Debug/myModule.node')
+} else {
+  myModule= require('./build/Release/myModule.node')
+}
+
+module.exports = myModule
